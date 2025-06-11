6830f73083e745368cde3fb4b2740667fec60f03
ByteDevX <153162567+joeyssn@users.noreply.github.com>
2025-06-12 00:07:24 +0700

    added watch app

---
+++ SportHubWatch Watch App/SportHubWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportHubWatchApp.swift
+//  SportHubWatch Watch App
+//
+//  Created by Kevin Christian on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SportHubWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
