e3d95fab8abe5c8e8b4b15b9fef7857f9140b8a2
Dannprand <dannielprand@gmail.com>
2025-06-12 10:17:34 +0700

    watch

---
+++ SporthubWatch Watch App/SporthubWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SporthubWatchApp.swift
+//  SporthubWatch Watch App
+//
+//  Created by student on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SporthubWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SporthubWatch Watch AppTests/SporthubWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SporthubWatch_Watch_AppTests.swift
+//  SporthubWatch Watch AppTests
+//
+//  Created by student on 12/06/25.
+//
+
+import Testing
+@testable import SporthubWatch_Watch_App
+
+struct SporthubWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
