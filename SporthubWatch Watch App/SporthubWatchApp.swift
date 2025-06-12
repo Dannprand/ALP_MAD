5749633d8c08cb5a619476a2d432cf8e8ebf3c53
Dannprand <dannielprand@gmail.com>
2025-06-12 10:21:45 +0700

    new watch os revision

--- SporthubWatch Watch App/SporthubWatchApp.swift
+++ SporthubWatch Watch App/SporthubWatchApp.swift
@@ -2,27 +2,16 @@
 //  SporthubWatchApp.swift
 //  SporthubWatch Watch App
 //
-//  Created by Kevin Christian on 12/06/25.
+//  Created by student on 12/06/25.
 //
 
 import SwiftUI
 
 @main
-struct SporthubWatchApp: App {
-    @StateObject private var connectivityManager = WatchConnectivityManager()
-    
+struct SporthubWatch_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(connectivityManager)
         }
     }
 }
-//@main
-//struct SporthubWatch_Watch_AppApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//        }
-//    }
-//}
